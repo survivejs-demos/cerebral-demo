ca65dfddbe0a498d9edceab0242caa1906305ea1
Juho Vepsalainen <bebraw@gmail.com>
2015-09-28 09:38:04 +0300

    Port actions to state api

--- kanban_app/app/index.jsx
+++ kanban_app/app/index.jsx
@@ -1,6 +1,7 @@
 import 'array.prototype.findindex';
 import './main.css';
 
+import uuid from 'node-uuid';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Container} from 'cerebral-react';
@@ -28,6 +29,44 @@ import controller from './libs/controller';
 
   }
 */
+
+// lanes
+controller.signal('laneCreated', (lane, state) => {
+  lane.id = uuid.v4();
+  lane.notes = lane.notes || [];
+
+  state.push('lanes', lane);
+});
+controller.signal('laneUpdated', ({id, name}, state) => {
+  state.set(['lanes', {id}, 'name'], name);
+});
+controller.signal('laneDeleted', (id, state) => {
+  state.unset(['lanes', {id}]);
+
+  // it would be a good idea to seek and destroy possible
+  // associated notes. now they remain hanging in the memory
+});
+
+// notes
+controller.signal('noteCreated', ({laneId, note}, state) => {
+  note.id = uuid.v4();
+
+  state.push('notes', note);
+
+  // it would be a good idea to check that the lane exists
+  // before doing this
+  state.push(['lanes', {id: laneId}, 'notes'], note.id);
+});
+controller.signal('noteUpdated', ({id, task}, state) => {
+  state.set(['notes', {id}, 'task'], task);
+});
+controller.signal('noteDeleted', ({laneId, noteId}, state) => {
+  state.unset(['lanes', {id: laneId}, 'notes', noteId]);
+  state.unset(['notes', {id: noteId}]);
+});
+controller.signal('noteMoved', ({sourceId, targetId}, state) => {
+  console.log('note moved', sourceId, targetId, state);
+});
 
 main();
 
--- kanban_app/app/libs/controller.js
+++ kanban_app/app/libs/controller.js
@@ -1,4 +1,3 @@
-import uuid from 'node-uuid';
 import Controller from 'cerebral';
 import Model from 'cerebral-baobab';
 
@@ -41,58 +40,9 @@ const model = Model({
   */
 });
 
-/*
-  CHRISTIAN: This file should only expose the controller
-  export default Controller(model);
-  Put your signals in the *index.jsx* file
-*/
-
 model.tree.on('update', (e) => {
   // TODO: write the new state to localStorage
   console.log('tree updated', e);
 });
 
-const controller = Controller(model);
-
-// lanes
-controller.signal('laneCreated', (lane) => {
-  lane.id = uuid.v4();
-  lane.notes = lane.notes || [];
-
-  model.tree.select('lanes').push(lane);
-});
-controller.signal('laneUpdated', ({id, name}) => {
-  model.tree.select('lanes', {id}).set('name', name);
-});
-controller.signal('laneDeleted', (id) => {
-  model.tree.select('lanes', {id}).unset();
-
-  // it would be a good idea to seek and destroy possible
-  // associated notes. now they remain hanging in the memory
-});
-
-// notes
-controller.signal('noteCreated', ({laneId, note}) => {
-  note.id = uuid.v4();
-
-  model.tree.select('notes').push(note);
-  const laneNotes = model.tree.select('lanes', {
-    id: laneId
-  }, 'notes');
-
-  // it would be a good idea to check that the lane exists
-  // before doing this
-  laneNotes.push(note.id);
-});
-controller.signal('noteUpdated', ({id, task}) => {
-  model.tree.select('notes', {id}).set('task', task);
-});
-controller.signal('noteDeleted', ({laneId, noteId}) => {
-  model.tree.select('lanes', {id: laneId}, 'notes', noteId).unset();
-  model.tree.select('notes', {id: noteId}).unset();
-});
-controller.signal('noteMoved', ({sourceId, targetId}) => {
-  console.log('note moved', sourceId, targetId);
-});
-
-export default controller;
+export default Controller(model);
