7dda892ea47fa8b0c4b40ded0e57db8b44c94786
Juho Vepsalainen <bebraw@gmail.com>
2015-09-28 10:09:18 +0300

    Simplify structure

--- kanban_app/app/index.jsx
+++ kanban_app/app/index.jsx
@@ -3,13 +3,28 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Controller from 'cerebral';
+import Model from 'cerebral-baobab';
 import {Container} from 'cerebral-react';
 import App from './components/App.jsx';
 import storage from './libs/storage';
-import controller from './libs/controller';
 
 import laneSignals from './signals/lane';
 import noteSignals from './signals/note';
+
+// TODO: load initial data from localStorage
+// it would be good to validate initial data now
+// if it's broken somehow, it's easy to end up with a broken app
+const model = Model({
+  lanes: [],
+  notes: []
+});
+model.tree.on('update', (e) => {
+  // TODO: write the new state to localStorage
+  console.log('tree updated', e);
+});
+
+const controller = Controller(model);
 
 laneSignals(controller);
 noteSignals(controller);
