0662d023817aa646635c25b21234dfedf8f11abe
christianalfoni <christianalfoni@gmail.com>
2015-09-28 07:55:21 +0200

    added comments

--- kanban_app/app/index.jsx
+++ kanban_app/app/index.jsx
@@ -8,6 +8,27 @@ import App from './components/App.jsx';
 import storage from './libs/storage';
 import controller from './libs/controller';
 
+/*
+  CHRISTIAN:
+  This is where you put your signals and import your actions. Actions
+  are named as "what they do"
+
+  import createLane from './actions/createLane.js';
+
+  controller.signal('laneCreated', createLane);
+
+  And this is how a typical action would look:
+
+  function createLane (input, state, output) {
+
+    state.push('lanes', {
+      id: uuid.v4(),
+      notes: input.lane.notes || []
+    });
+
+  }
+*/
+
 main();
 
 function main() {
--- kanban_app/app/libs/controller.js
+++ kanban_app/app/libs/controller.js
@@ -4,9 +4,48 @@ import Model from 'cerebral-baobab';
 
 // TODO: load the initial state of the app from localStorage here
 const model = Model({
+
   lanes: [],
-  notes: []
+  notes: [],
+
+  /*
+    CHRISTIAN:
+    // Typically you use objects to store data and use
+    // monkeys to display that data
+    lanes: {},
+    notes: {},
+
+    // Not knowing exactly how the data relates, this is how
+    // lanes could displays notes
+    currentLaneId: null, // This state defines active line using ID of it
+    currentLane: Model.monkey({
+      cursors: {
+        id: ['currentLaneId'],
+        lanes: ['lanes'],
+        notes: ['notes']
+      },
+      get(data) {
+        if (!data.id) {
+          return null;
+        }
+
+        const lane = data.lanes[data.id];
+
+        return {
+          name: lane.name,
+          notes: lane.notes.map((noteId) => data.notes[nodeId])
+        };
+      }
+    })
+
+  */
 });
+
+/*
+  CHRISTIAN: This file should only expose the controller
+  export default Controller(model);
+  Put your signals in the *index.jsx* file
+*/
 
 model.tree.on('update', (e) => {
   // TODO: write the new state to localStorage
